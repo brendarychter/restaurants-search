3bb49c013f5032502fd7e18b8436039e9d711466
brenda.rychter <brendarychter@gmail.com>
2023-04-29 23:19:43 -0300

    add new configuration

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig(({ command, mode }) => {
   const env = loadEnv(mode, process.cwd(), '')
+  console.log(command)
   return {
     plugins: [react()],
     define: {
